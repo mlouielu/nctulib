6e4fd18b94f98f1f3ecec15a91907b6a86a1c2a0
Louie Lu <git@louie.lu>
2017-09-30 21:19:30 +0800

    Add simple API

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+from nctulib import library
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['JSON_AS_ASCII'] = False
+
+
+class BookSearch(Resource):
+    lib = library.NCTULibrary()
+
+    def get(self, q):
+        return jsonify([e.json() for e in self.lib.search(q)])
+
+
+class BookLocation(Resource):
+    lib = library.NCTULibrary()
+
+    def get(self, bid):
+        return jsonify(self.lib.get_locations_by_bid(bid))
+
+
+api.add_resource(BookSearch, '/books/<string:q>')
+api.add_resource(BookLocation, '/location/<string:bid>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
